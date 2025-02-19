d02151b1e25029ec8b9123fe02ae8214e747e63d
duyyyyyyyyy <duytnps41276@gmail.com>
2025-02-19 22:39:03 +0700

    duy update

---
+++ Assets/AssetDuy/Script/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Player Movement
+    public float movementSpeed = 5f;
+    private CharacterController controller;
+
+    //Gravity
+    public float gravity = -9.81f;
+    public Transform groundCheck;
+    public LayerMask groundMask;
+    public float groundDistance = 0.4f;
+    private bool isGrounded;
+    private Vector3 velocity;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = 2f;
+        }
+        HandleMovement();
+        HandleGravity();
+        controller.Move(velocity * Time.deltaTime);
+    }
+    void HandleMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = transform.right * horizontalInput + transform.forward * verticalInput;
+        movement.y = 0;
+        controller.Move(movement * movementSpeed * Time.deltaTime);
+    }
+    void HandleGravity()
+    {
+        velocity.y += gravity * Time.deltaTime;
+    }
+}
