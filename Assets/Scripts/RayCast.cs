c612cebdc4c645b67b17b538fb3e761f3c07364c
tiendat1708 <datttps41395@gmail.com>
2025-02-24 03:11:43 +0700

    loi len

---
+++ Assets/Scripts/RayCast.cs
@@ -0,0 +1,33 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI; // Để sử dụng Text UI cho việc hiển thị "F"
+
+public class RayCast : MonoBehaviour
+{
+    [SerializeField]
+    private LayerMask layerMask;
+    private int CongItem = 0;
+    [SerializeField]
+    private TextMeshProUGUI ItemCong;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        // Kiểm tra va chạm với vật phẩm trong phạm vi raycast
+        if (Physics.Raycast(transform.position, transform.forward, out var hit, 1, layerMask))
+        {
+            Debug.DrawRay(transform.position, transform.forward * hit.distance, Color.red);
+            Destroy(hit.transform.gameObject);
+            CongItem++;
+            ItemCong.text = "Item:" + CongItem;
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.forward * 1, Color.yellow);
+        }
+    }
+}
